643ffca2387e4f2d97790eae1e226980782d573e
Jessica <yanzeliu.france@gmail.com>
2017-03-29 10:08:16 -0700

    initial commit

---
+++ Flicks/APIManager.swift
@@ -0,0 +1,13 @@
+//
+//  APIManager.swift
+//  Flicks
+//
+//  Created by yanze on 3/29/17.
+//  Copyright © 2017 yanzeliu. All rights reserved.
+//
+
+import Foundation
+
+class APImanager: NSObject {
+    
+}
